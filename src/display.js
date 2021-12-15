c5f400182db9fa3331c32438aed564d63f75a860
od-c0d3r <omar_rashad@aol.com>
2021-12-15 20:20:38 +0200

    Feat: update table after submission

--- src/display.js
+++ src/display.js
@@ -1,23 +1,34 @@
 import { getScores } from './api.js';
 
-function compare( a, b ) {
-  if ( +a.score < +b.score ){
+function compare(a, b) {
+  if (+a.score < +b.score) {
     return -1;
   }
-  if ( +a.score > +b.score ){
+  if (+a.score > +b.score) {
     return 1;
   }
   return 0;
 }
 
-export default function showData(data) {
+export function updateTable(data) {
+  const table = document.getElementById('table');
+  const score = document.createElement('tr');
+  score.innerHTML = `
+            <td>${data.user}</td>
+            <td>${data.score}</td>
+        `;
+  table.appendChild(score);
+}
+
+export function showData(data, sorted = false) {
+  let arr = sorted ? data.sort(compare) : data;
   const table = document.getElementById('table');
   table.innerHTML = `
     <tr>
       <th>Name</th>
       <th>Score</th>
     </tr>`;
-  data.sort(compare).forEach((scoreObj) => {
+  arr.forEach((scoreObj) => {
     const score = document.createElement('tr');
     score.innerHTML = `
             <td>${scoreObj.user}</td>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import './style.scss';
 import './darkmode.js';
-import showData from './display.js';
 import { getScores, postScore } from './api.js';
+import { showData, updateTable } from "./display.js";
 
 document.addEventListener('submit', (e) => {
   const [name, score, errors] = ['nameInput', 'scoreInput', 'errors'].map((id) => document.getElementById(id));
   if (score.value > 0) {
-    postScore({ user: name.value, score: score.value })
+    postScore({ user: name.value, score: score.value });
+    updateTable({ user: name.value, score: score.value })
     document.forms[0].reset();
   } else {
     errors.innerHTML = '* Please, enter new score';
@@ -16,4 +17,5 @@ document.addEventListener('submit', (e) => {
 
 document.addEventListener('click', (e) => {
   if (e.target.id === 'refreshBtn') getScores().then((data) => showData(data.result));
+  if (e.target.id === 'sortBtn') getScores().then((data) => showData(data.result, true));
 });
