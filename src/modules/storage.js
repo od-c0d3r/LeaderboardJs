c1a93dc0dc2ce9f899c79c73dcc4706b61dd8295
od-c0d3r <omar_rashad@aol.com>
2021-12-16 12:12:57 +0200

    Feat: new game every 1st time

---
+++ src/modules/storage.js
@@ -0,0 +1,8 @@
+export function setGameLocal(id) {
+    localStorage.setItem('gameId', id);
+    return true
+}
+
+export function getGameIdLocal() {
+    return localStorage.getItem('gameId') || '';
+}
