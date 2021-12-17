840314853a8621d75bb0ba1042767d15f831b7cf
od-c0d3r <omar_rashad@aol.com>
2021-12-17 12:04:45 +0200

    Code: display.js optimized

--- src/modules/display.js
+++ src/modules/display.js
@@ -8,34 +8,36 @@ function compare(a, b) {
   return 0;
 }
 
-function getTable() {
-  return document.getElementById('table');
+function getElementWith(id) {
+  return document.getElementById(id);
+}
+
+function createElementWith(id) {
+  return document.createElement(id);
+}
+
+function htmlScore(obj) {
+  return `<td>${obj.user}</td><td>${obj.score}</td>`
 }
 
 export function updateTable(data) {
-  const table = getTable();
-  const score = document.createElement('tr');
-  score.innerHTML = `
-            <td>${data.user}</td>
-            <td>${data.score}</td>
-        `;
+  const table = getElementWith('table');
+  const score = createElementWith('tr')
+  score.innerHTML = htmlScore(data);
   table.appendChild(score);
 }
 
 export function showData(data, sorted = false) {
   const arr = sorted ? data.sort(compare) : data;
-  const table = getTable();
+  const table = getElementWith('table');
   table.innerHTML = `
     <tr>
       <th>Name</th>
       <th>Score</th>
     </tr>`;
   arr.forEach((scoreObj) => {
-    const score = document.createElement('tr');
-    score.innerHTML = `
-            <td>${scoreObj.user}</td>
-            <td>${scoreObj.score}</td>
-        `;
+    const score = createElementWith('tr');
+    score.innerHTML = htmlScore(scoreObj)
     table.appendChild(score);
   });
 }
